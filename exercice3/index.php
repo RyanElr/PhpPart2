8097c9e118b098ba6523041fd7d929ff2a2f0e2d
Ryan <ryan.elr@outlook.com>
2018-07-26 15:33:02 +0200

    new

--- exercice3/index.php
+++ exercice3/index.php
@@ -11,20 +11,19 @@ $genre = 'homme';
   <body>
     <p>
       <?php
-      if ($age <1) {
+      if ($age <1 && $genre == 'homme' && $genre == 'femme') {
         echo('Ahahaha très drôle, vous n\'êtes pas encore né(e)');
         // code...
       }
-      elseif ($age >=130) {
+      elseif ($age >=130 && $genre == 'homme' && $genre == 'femme') {
         echo('Comment peut-on se connecter à partir d\'une tombe?');
         // code...
       }
-      elseif ($age >=110) {
+      elseif ($age >=110 && $genre == 'homme' && $genre == 'femme') {
         echo('Vous ne pouvez pas être encore en vie !! Vraiment tenace');
         // code...
       }
-
-      elseif ($age >= 90) {
+      elseif ($age >= 90 && $genre == 'homme' && $genre == 'femme') {
         echo('Vraiment capable de se servir d\'un ordinateur?');
         // code...
       }
@@ -39,6 +38,9 @@ $genre = 'homme';
       }
       elseif ($age >= 18 && $genre == 'femme') {
         echo ('Vous êtes une femme et vous êtes majeure');
+      }
+      else {
+        echo ('Un ovni?');
       }
       ?>
     </p>
